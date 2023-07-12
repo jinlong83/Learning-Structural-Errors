961647753561aba36cb57de7db33e3fb08672c9a
mattlevine22 <mattlevine22@gmail.com>
2023-07-12 18:32:44 +0200

    updating main_nn.py for Daniel debug runs. It will only run joint EKI (learn ICs + parameters)---no DA. It sets tp parameter to truth (i.e. NO MODEL ERROR), so goal is to learn a NN == 0 (this simplifies initialization and interpretation). Note that Vg=0.1 for better coordinate scaling.

--- sandbox/EKI_with_DA/main_nn.py
+++ sandbox/EKI_with_DA/main_nn.py
@@ -45,26 +45,30 @@ def run_all(MODELNAME='L63', meta_dir='results', seed=0):
             'integrator': 'RK45'
         }
     elif MODELNAME == 'UltradianGlucoseModel':
-        DAsteps = 10    #EKI steps
-        nSamples = 50 #20   #Ensemble size (need more particles for NN w/ 26 params)
+        DAsteps = 5    #EKI steps
+        nSamples = 30 #20   #Ensemble size (need more particles for NN w/ 26 params)
 
         DRIVER = np.array(pd.read_csv('../../data/P1_nutrition_expert.csv'))
         PARAM_NAMES = [] # list of parameter names to be learned
         DT = 20.0 # data timestep (not an integration timestep)
-        # T_RANGE = np.arange(3500, 6000, DT) # np.arange(3500, 10000, DT) # generate data every 20 minutes
-        T_RANGE = np.arange(3500, 4500, DT) # np.arange(3500, 10000, DT) # generate data every 20 minutes
+        T_RANGE = np.arange(3500, 6000, DT) # np.arange(3500, 10000, DT) # generate data every 20 minutes
+        # T_RANGE = np.arange(3500, 4500, DT) # np.arange(3500, 10000, DT) # generate data every 20 minutes
         T_DA = int(0.4*len(T_RANGE)) # number of measurements to use in assimilation warmup phase
         HOBS = np.eye(3) #np.array([[0, 0, 1]])
-        IC_TRUE = np.array([50, 50, 10000]) # + np.random.randn(3)
-        x_ic_mean = np.array([55, 130, 13000])
+        IC_TRUE = np.array([50., 50., 100.00]) # + np.random.randn(3)
+        # print('WARNING: using downscaled IC for UltradianGlucoseModel')
+        # IC_TRUE = np.array([5, 5, 10])  # + np.random.randn(3)
+        x_ic_mean = np.array([55., 130., 130.00])
+        # x_ic_mean = np.array([5.5, 13.0, 13.000])
+
         state_noise_cov = np.diag((0.01*(x_ic_mean)) ** 2)
         # ODE_SETTINGS_APPROX = {'Um': 0, 'U0': 0, 'nn_dims': [3, 25, 1]}
         ODE_SETTINGS_APPROX = {
                                 # 'Rg': 0,
-                               'tp': np.inf, 
-                            #    'nn_dims': [3, 5, 1], 
+                            #    'tp': np.inf, 
+                               'nn_dims': [3, 5, 1], 
                             #    'constrain_positive': False,
-                               'nn_dims': [3, 1], 
+                            #    'nn_dims': [3, 1], 
                                 'nn_rescale_input': False,
                                 'nn_rescale_output': False}
         ODE_SETTINGS_TRUE = {}
@@ -154,30 +158,30 @@ def run_all(MODELNAME='L63', meta_dir='results', seed=0):
 
     ## Run experiments:
     # # run enkf
-    experiment['enkf'].run()
+    # experiment['enkf'].run()
 
     # Run 3dvar
-    experiment['3dvar'].run()
+    # experiment['3dvar'].run()
 
     # run joint
-    # experiment['joint'].run()
+    experiment['joint'].run()
 
     # run joint for longer
-    experiment['joint'].normalizer = NORMALIZER
-    experiment['joint'].nSamples = DA_SETTINGS['N_particles']*nSamples
-    experiment['joint'].output_dir = os.path.join(meta_dir,'partial_noisy_jointEKI_moreParticles')
-    experiment['joint'].run()
+    # experiment['joint'].normalizer = NORMALIZER
+    # experiment['joint'].nSamples = DA_SETTINGS['N_particles']*nSamples
+    # experiment['joint'].output_dir = os.path.join(meta_dir,'partial_noisy_jointEKI_moreParticles')
+    # experiment['joint'].run()
 
 
 if __name__ == "__main__":
-    meta_dir = 'results_June30_2023/PartialObs_LowNoise_v1'
-    for seed in [1,2]:
-        print('Running L63 experiments...')
-        run_all('L63', meta_dir=os.path.join(
-            meta_dir, f'L63_{seed}'), seed=seed)
+    # meta_dir = 'results_July4_2023_rcond1e-7/PartialObs_LowNoise_v1_tryRescale'
+    # for seed in [1,2]:
+    #     print('Running L63 experiments...')
+    #     run_all('L63', meta_dir=os.path.join(
+    #         meta_dir, f'L63_{seed}'), seed=seed)
 
     # meta_dir = 'results_NN_1stepahead_fixedt0Bug/run_tpInf_trueIC_fullObs_v2'
-    meta_dir = 'results_June30_2023/FullObs_LowNoise_v0_debug_tpInf'
+    meta_dir = 'results_July12_2023/FullObs_LowNoise_goodIC_tpTrue_NN1.0/Daniel_debugging'
     for seed in [0,1]:
         print('Running Ultradian experiments...')
         run_all('UltradianGlucoseModel', meta_dir=os.path.join(
